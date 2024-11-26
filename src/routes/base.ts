0124f7f5151ebd8281f5995feceb3443eba1c066
Sergio Acosta <sergioacostaquintana@gmail.com>
2024-11-26 21:03:29 +0100

    todo

--- src/routes/base.ts
+++ src/routes/base.ts
@@ -1,25 +1,6 @@
-import { scrapSpotify } from "@/utils/scrapers";
 import { Request, Response, Router } from "express";
 
 export const base = (router: Router) => {
-  // router.get("/", async (_: Request, res: Response) => {
-  //   try {
-  //     const data = await scrapSpotify(
-  //       "https://open.spotify.com/artist/3IfsgBb93KlSIBNVQOIsHH"
-  //     );
-
-  //     res.json({
-  //       success: true,
-  //       data,
-  //     });
-  //   } catch (error:any) {
-  //     console.error("Error in scrapSpotify:", error.message);
-  //     res.status(500).json({
-  //       success: false,
-  //       message: "Failed to fetch Spotify data.",
-  //     });
-  //   }
-  // });
   router.get("/", async (_: Request, res: Response) => {
     try {
       res.send(process.env.APP_URL);
