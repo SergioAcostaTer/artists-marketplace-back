a0b812ce2d753f9f932a03f6c1731da0233d6ccc
Sergio Acosta <sergioacostaquintana@gmail.com>
2024-11-26 09:05:25 +0100

    deployed

--- src/routes/base.ts
+++ src/routes/base.ts
@@ -22,7 +22,7 @@ export const base = (router: Router) => {
   // });
   router.get("/", async (_: Request, res: Response) => {
     try {
-      res.send(process.env.JWT_SECRET);
+      res.send(process.env.APP_URL);
     } catch (error: any) {
       res.status(500).send("Error");
     }
